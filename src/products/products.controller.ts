f34bd0ed8b242fc0271fb72bfb4a6fdbe13edf7b
22051988Di <vitalii.soloninko@gmail.com>
2024-05-31 18:10:01 +0200

    v0.0.4	Get a request for one product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getAll() {
+  getAll(): string {
     return 'getAll';
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): string {
+    return 'getOne ' + id;
   }
 }
