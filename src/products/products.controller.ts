993d20f816ebfb1ec2da47e57330d32ca8fbae91
22051988Di <vitalii.soloninko@gmail.com>
2024-06-04 19:16:15 +0200

    v0.0.11	Dynamic product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
 
 // express
 // app.use((req, res, next) => {
@@ -20,6 +21,8 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
   // @Get()
   // // @Redirect('https://google.com', 301)
   // getAll(@Req() req: Request, @Res() res: Response): string {
@@ -28,20 +31,20 @@ export class ProductsController {
   // }
 
   @Get()
-  getAll(): string {
-    return 'getAll';
+  getAll() {
+    return this.productsService.getAll();
   }
 
   @Get(':id')
   getOne(@Param('id') id: string): string {
-    return 'get by id ' + id;
+    return this.productsService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createProductDto: CreateProductDto): string {
-    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`;
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
 
   @Delete(':id')
