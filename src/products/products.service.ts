f29b6d7539b6bc0ee43f9c9deaa92b1cc34a9412
22051988Di <vitalii.soloninko@gmail.com>
2024-06-05 12:55:31 +0200

    v0.0.19	Logic in service, import model

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
+import { Product, ProductDocument } from './schemas/product.schema';
 
 @Injectable()
 export class ProductsService {
-  private products = [];
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
 
   getAll() {
     return this.products;
