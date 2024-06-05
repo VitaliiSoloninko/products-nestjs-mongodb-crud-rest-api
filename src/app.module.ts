87216b6c4a899f6a2025efa1f04c7fbc1e444f89
22051988Di <vitalii.soloninko@gmail.com>
2024-06-05 12:50:28 +0200

    v0.0.18	Connect with base in app module login password /address

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://vitaliypulupovuch:YPvglwuVjYpOgjsy@nest-api.d1cwop9.mongodb.net/products`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
