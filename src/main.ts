029c3badc642f76cc62dca06c04e853fd236f79c
22051988Di <vitalii.soloninko@gmail.com>
2025-01-30 09:10:08 +0100

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import * as process from 'process';
+// import * as process from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, '0.0.0.0');
+  // await app.listen(process.env.PORT, '0.0.0.0');
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
